8d57b9b39e7eaa3c63a9c7ddb19fbd90719b5c93
Ivan Gregoretti <ivangreg@gmail.com>
2017-04-11 09:24:00 -0400

    First commit.

---
+++ createbucket.py
@@ -0,0 +1,265 @@
+#!/usr/bin/python3
+# Ivan Gregoretti, PhD. April 2017.
+
+import subprocess
+import boto3
+import json
+import jmespath
+import datetime # Usage: execute datetime.datetime.now(datetime.timezone.utc); then instead of tzinfo=tzutc() use tzinfo=datetime.timezone.utc.
+import pandas as pd
+
+
+
+################################################################################
+# Create session and identify user (self)
+################################################################################
+
+# Create custom session
+se1 = boto3.Session( profile_name='default' )    # profile: default
+#se2= boto3.Session( profile_name='ivan'    )    # profile: ivan
+
+
+# Create Identity and Access Managemet resource and client
+iamre = se1.resource('iam')
+iamcl = se1.client(  'iam')
+
+# Identify my user name
+iam_user_name = jmespath.search('Users[*] | [?UserName!=`null`] | [?contains(UserName, `Gregoretti`) || contains(UserName, `gregoretti`)].UserName | [0]', iamcl.list_users())
+iam_user_id   = jmespath.search('Users[*] | [?UserName!=`null`] | [?contains(UserName, `Gregoretti`) || contains(UserName, `gregoretti`)].UserId   | [0]', iamcl.list_users())
+
+
+################################################################################
+# Create Resources and Client for interation with S3 and IAM
+################################################################################
+
+# Create an S3 resource and/or client
+s3re = se1.resource('s3')
+s3cl = se1.client(  's3')
+
+# In case it is needed, this is how to display the canonical user ID of this acccount
+s3cl.list_buckets()['Owner']
+
+
+##############################
+# Convenience function to avoid the problem of "datetime ... is not JSON serializable".
+##############################
+
+def date_handler(obj):
+    if hasattr(obj, 'isoformat'):
+        return obj.isoformat()
+    else:
+        raise TypeError
+
+# Usage: json.dumps(s3cl.list_buckets(), default=date_handler)
+# or json.dumps(s3cl.list_buckets()['Buckets'], default=date_handler)
+# Note: Pretty print like this: print(json.dumps(s3cl.list_buckets()['Buckets'], default=date_handler, indent=4))
+
+
+
+################################################################################
+# Create bucket and the perception of directories
+################################################################################
+
+# Create a bucket in the S3 resource
+my_s3bucket = s3re.create_bucket(ACL='private', Bucket='cst-compbio-research-00-buc') # Bug in the S3 API. Do nto specidy region if us-east-1 is desired.
+#my_s3bucket = s3re.create_bucket(ACL='private', Bucket='cst-compbio-research-00-buc', CreateBucketConfiguration={'LocationConstraint': 'us-east-1'})
+
+
+# Tag immediately
+s3re.BucketTagging( bucket_name=my_s3bucket.name ).put( Tagging={'TagSet': [
+    {'Key':'Name', 'Value':'cst-compbio-research-00-buc'},
+    {'Key':'Owner', 'Value':iam_user_name},
+    {'Key':'Department', 'Value':'Computational Biology Research'}
+]})
+
+
+# Create directories
+my_s3bucket.put_object(Key='home/')
+my_s3bucket.put_object(Key='snapshots/')
+my_s3bucket.put_object(Key='scratch/')
+my_s3bucket.put_object(Key='tmp/')
+
+
+# Create subdirectories
+my_s3bucket.put_object(Key='home/ivan.gregoretti@cellsignal.com/')
+my_s3bucket.put_object(Key='home/stephen.brinton@cellsignal.com/')
+my_s3bucket.put_object(Key='home/sean.landry@cellsignal.com/'    )
+my_s3bucket.put_object(Key='snapshots/ivan.gregoretti@cellsignal.com/')
+my_s3bucket.put_object(Key='snapshots/stephen.brinton@cellsignal.com/')
+my_s3bucket.put_object(Key='snapshots/sean.landry@cellsignal.com/'    )
+my_s3bucket.put_object(Key='scratch/ivan.gregoretti@cellsignal.com/')
+my_s3bucket.put_object(Key='scratch/stephen.brinton@cellsignal.com/')
+my_s3bucket.put_object(Key='scratch/sean.landry@cellsignal.com/'    )
+
+
+
+################################################################################
+# Create the bucket's access policy
+################################################################################
+
+# Note: S3 actions and examples are documented here:
+# http://docs.aws.amazon.com/AmazonS3/latest/dev/using-with-s3-actions.html
+# https://aws.amazon.com/blogs/security/writing-iam-policies-grant-access-to-user-specific-folders-in-an-amazon-s3-bucket/
+# Note: The "Principal":"*" component has been removed from the statement because
+# in IAM policies it is implicitly assumed that the Principal is the user to
+# whom the policy is attached.
+my_policy_json = json.dumps(
+{
+    "Version":"2012-10-17",
+    "Statement": [
+        {
+            "Sid": "1AllowSeeingBucketsList",
+            "Effect": "Allow",
+            "Action": [ "s3:ListAllMyBuckets", "s3:GetBucketLocation" ],
+            "Resource": [ "arn:aws:s3:::*" ]
+        },
+        {
+            "Sid": "2AllowRootLevelListingOfThisBucketAndItsDirectories",
+            "Effect": "Allow",
+            "Action": [ "s3:ListBucket" ],
+            "Resource": [ "arn:aws:s3:::" + my_s3bucket.name ],
+            "Condition": {
+                "StringLike": {
+                    "s3:prefix": ["", "home/", "home/${aws:username}/", "snapshots/", "snapshots/*", "scratch/", "scratch/*", "tmp/", "tmp/*"],
+                    "s3:delimiter": [ "/" ]
+                }
+            }
+        },
+        {
+            "Sid": "3AllowListingAndGetAndPutAndDelTmp",
+            "Effect": "Allow",
+            "Action": [ "s3:GetObject", "s3:GetObjectTagging", "s3:PutObject", "s3:PutObjectTagging" , "s3:DeleteObject", "s3:DeleteObjectTagging"],
+            "Resource": [ "arn:aws:s3:::" + my_s3bucket.name + "/tmp/*" ]
+        },
+        {
+            "Sid": "4AllowGetSnapshotsAndScratch",
+            "Effect": "Allow",
+            "Action": [ "s3:GetObject", "s3:GetObjectTagging", "s3:GetObjectVersion", "s3:GetObjectVersionTagging" ],
+            "Resource": [ "arn:aws:s3:::" + my_s3bucket.name + "/snapshots/*", "arn:aws:s3:::" + my_s3bucket.name + "/scratch/*" ]
+        },
+        {
+            "Sid": "5AllowEverythingInUsersOwnHomeAndSnapshotsAndScratch",
+            "Effect": "Allow",
+            "Action": [ "s3:*" ],
+            "Resource": [ "arn:aws:s3:::" + my_s3bucket.name + "/home/${aws:username}/*", "arn:aws:s3:::" + my_s3bucket.name + "/snapshots/${aws:username}/*", "arn:aws:s3:::" + my_s3bucket.name + "/scratch/${aws:username}/*" ]
+        }
+    ]
+}
+, indent=4)
+
+my_iampolicy = iamre.create_policy(PolicyName='compbio-research-s3-00-pol', PolicyDocument=my_policy_json, Description='Computational Biology Research S3 access control.' )
+
+
+########################################
+# Creating a new version of an existing policy
+########################################
+
+# Policy documents are organised and indexed by verion (eg v1, v2, v3, etc).
+# One version is considered the default policy version.
+
+# Here it is assumed that a new my_policy_json document has been created.
+# Add it as non-default.
+my_iampolicy.create_version(PolicyDocument=my_policy_json, SetAsDefault=False)
+
+# Show versions and report whether or not it is the default
+for x in my_iampolicy.versions.all(): print(x.version_id, x.is_default_version)
+
+
+########################################
+# Deleting a policy, if needed.
+########################################
+
+# When there are multiple versions, all non-default versions of the policy must
+# be deleted before the policy in its entirety can be deleted.
+
+
+####################
+# Deleting a policy when there are multiple versions
+####################
+
+# Show versions and report whether or not it is the default
+for x in my_iampolicy.versions.all(): print(x.version_id, x.is_default_version)
+
+# Display the document of the policy (examples)
+#     iamre.PolicyVersion(arn=my_iampolicy.arn, version_id='v1').document
+#     iamre.PolicyVersion(arn=my_iampolicy.arn, version_id='v2').document
+
+# Assuming that v2 is not the default version, delete version v2.
+#     iamcl.delete_policy_version(PolicyArn=my_iampolicy.arn, VersionId='v2')
+
+
+####################
+# Deleting a policy when there is only one version (the default one)
+####################
+
+iamcl.delete_policy(PolicyArn=my_iampolicy.arn)
+
+
+
+################################################################################
+# Attach Policy to a Group of users
+################################################################################
+
+#iamre.Group(name='compbio-research-group').attach_policy(PolicyArn=my_iampolicy.arn)
+# Note: The command above is not executed at the moment because the members of
+# this group already have AmazonS3FullAccess.
+
+
+### DONE ###
+
+
+
+################################################################################
+# Appendix 1. Delete a non-empty bucket.
+################################################################################
+
+# List content of the bucket
+for obj in my_s3bucket.objects.all(): print(obj.key)
+
+# Delete the objects inside the bucket
+for obj in my_s3bucket.objects.all(): obj.delete()
+
+# Delete the bucket itself.
+my_s3bucket.delete()
+
+# Bonus. This is how to list all objects in all bucket. Warning: big output.
+#for bucket in s3re.buckets.all():
+#    for obj in bucket.objects.all():
+#        print(obj.key)
+
+
+
+################################################################################
+# Appendix 2. Pretty print JSON documents at the BASH command line.
+################################################################################
+
+# Note: the BASH command line is less flexible than ipython3 command line. BASH accepts only double quotes inside the expression.
+
+# Example
+# echo '{"Version":"2012-10-17","Statement":[{"Effect":"Deny","Principal":"*","Action":"s3:*","Resource":["arn:aws:s3:::my-bucket-12345","arn:aws:s3:::my-bucket-12345/*"],"Condition":{"StringNotLike":{"aws:userId":["AIDAJ4ICC4DFD5DCGTM5C"]}}}]}'  | python3 -m json.tool
+
+#{
+#    "Version": "2012-10-17",
+#    "Statement": [
+#        {
+#            "Effect": "Deny",
+#            "Principal": "*",
+#            "Action": "s3:*",
+#            "Resource": [
+#                "arn:aws:s3:::my-bucket-12345",
+#                "arn:aws:s3:::my-bucket-12345/*"
+#            ],
+#            "Condition": {
+#                "StringNotLike": {
+#                    "aws:userId": [
+#                        "AIDAJ4ICC4DFD5DCGTM5C"
+#                    ]
+#                }
+#            }
+#        }
+#    ]
+#}
+
+
+
+
---
+++ createefs.py
@@ -0,0 +1,173 @@
+#!/usr/bin/python3
+# Ivan Gregoretti, PhD. March 2017.
+
+import subprocess
+import boto3
+import json
+import jmespath
+import datetime # Usage: execute datetime.datetime.now(datetime.timezone.utc); then instead of tzinfo=tzutc() use tzinfo=datetime.timezone.utc.
+import pandas as pd
+
+pd.set_option('display.width', 270)
+
+
+################################################################################
+# Create session and identify relevant VPC and subnet
+################################################################################
+
+# Create custom session
+se1 = boto3.Session( profile_name='default' )    # profile: default
+#se2= boto3.Session( profile_name='ivan'    )    # profile: ivan
+
+
+# Create Identity and Access Managemet resource and client
+iamre = se1.resource('iam')
+iamcl = se1.client(  'iam')
+
+# Identify my user name
+iam_user_name = jmespath.search('Users[*] | [?UserName!=`null`] | [?contains(UserName, `Gregoretti`) || contains(UserName, `gregoretti`)].UserName | [0]', iamcl.list_users())
+
+# Create an EC2 resource and/or client
+ec2re = se1.resource('ec2')
+ec2cl = se1.client(  'ec2')
+
+
+# Creating an EFS requires identification of the VPC and its subnets.
+# The VPC is required to create the Security Groups.
+# The subnets are required to define the mount targets.
+
+# Identify a relevant VPC in which to work
+# Note: The idea is to first and quickly avoid the default VPC and then search for a good VPC by tags
+my_vpcid = jmespath.search(
+        'Vpcs[?!( IsDefault )] | [?Tags[?Key==`Name` && Value==`CSTSANDBOXVPC`]].VpcId',
+        ec2cl.describe_vpcs()
+        )[0]
+
+# Note: Construct a dataframe with relevant subnets organised by availability zone (rows) and exposure (columns)
+my_subnetid_df = pd.DataFrame({'Public':['', ''], 'Private':['', '']}, index=['us-east-1a', 'us-east-1e'])
+my_subnetid_df.loc['us-east-1a','Public' ] = jmespath.search('Subnets[?VpcId==`' + my_vpcid + '`] | [?Tags[?Key==`Name` && Value==`Public1A subnet` ]].SubnetId | [0]', ec2cl.describe_subnets())
+my_subnetid_df.loc['us-east-1a','Private'] = jmespath.search('Subnets[?VpcId==`' + my_vpcid + '`] | [?Tags[?Key==`Name` && Value==`Private1A subnet`]].SubnetId | [0]', ec2cl.describe_subnets())
+my_subnetid_df.loc['us-east-1e','Public' ] = jmespath.search('Subnets[?VpcId==`' + my_vpcid + '`] | [?Tags[?Key==`Name` && Value==`Public1E subnet` ]].SubnetId | [0]', ec2cl.describe_subnets())
+my_subnetid_df.loc['us-east-1e','Private'] = jmespath.search('Subnets[?VpcId==`' + my_vpcid + '`] | [?Tags[?Key==`Name` && Value==`Private1E subnet`]].SubnetId | [0]', ec2cl.describe_subnets())
+# For example:
+#                     Private           Public
+# us-east-1a  subnet-d00ed699  subnet-d30ed69a
+# us-east-1e  subnet-9701ecab  subnet-2e01ec12
+#
+# Elements can be selected by index, like this:
+# my_subnetid_df.loc['us-east-1a','Public']
+
+
+
+################################################################################
+# Identify or create security group
+################################################################################
+
+# Identify my own IP address
+# Note: This runs as a OS process and it is used to figure out what local public IP internet services can see.
+my_ip_child = subprocess.run(['curl', '--silent', 'http://checkip.amazonaws.com'], stdout=subprocess.PIPE)
+my_ip       = my_ip_child.stdout.decode('utf-8').rstrip()
+
+
+# Determine whether the compbio-research-00-sg security group exists
+my_security_group_name = 'compbio-research-00-sg'
+
+if my_security_group_name in jmespath.search('SecurityGroups[*].GroupName', ec2cl.describe_security_groups()):
+
+    print('Security group ' + my_security_group_name + ' already exists.')
+
+    # get the security group id (string)
+    my_security_group_id = jmespath.search('SecurityGroups[?GroupName==`' + my_security_group_name + '`].GroupId',
+            ec2cl.describe_security_groups(Filters=[{'Name':'vpc-id', 'Values':[my_vpcid] }])
+            )[0]
+
+    # get the security group (resource object)
+    my_security_group = ec2re.SecurityGroup( my_security_group_id   )
+
+else:
+
+    print('Security group ' + my_security_group_name + ' does not exist. Creating it...')
+
+    # Create a security group
+    my_security_group = ec2re.create_security_group(GroupName=my_security_group_name, Description='Allows access to Computational Biology Research', VpcId=my_vpcid)
+    # Add a tag
+    my_security_group.create_tags(Tags=[{'Key': 'Name', 'Value': my_security_group_name + '-name'}, {'Key':'Owner', 'Value':iam_user_name}, {'Key': 'Department', 'Value': 'Computational Biology Research'}])
+    # Set security group attributes
+    my_security_group.authorize_ingress(IpPermissions=[{'IpProtocol':'tcp', 'FromPort':22, 'ToPort':22, 'IpRanges':[{'CidrIp':'10.10.6.0/24'},{'CidrIp': my_ip + '/32'}]}])
+
+    print('Security group ' + my_security_group.group_name + ' has now been created.')
+
+
+# If desired, delete the security group
+#ec2cl.delete_security_group(GroupId=my_security_group.id)
+
+
+
+################################################################################
+# Create EFS
+################################################################################
+
+# Create an EFS client.
+# Note: there is only a Client interface with AWS, no Resources interface at the
+# time of this writing.
+efscl = se1.client('efs')
+
+# Define EFS name and CreationToken strings
+my_efs_name = 'compbio-research-00-efs'
+my_efs_creation_token = my_efs_name + '-token'
+
+try:
+    my_efs_id = efscl.describe_file_systems(CreationToken=my_efs_creation_token)['FileSystems'][0]['FileSystemId']
+
+except:
+
+    efscl.create_file_system(CreationToken=my_efs_creation_token, PerformanceMode='generalPurpose')
+    my_efs_id = efscl.describe_file_systems(CreationToken=my_efs_creation_token)['FileSystems'][0]['FileSystemId']
+    # Tag the newly created EFS
+    efscl.create_tags(FileSystemId=my_efs_id, Tags=[{'Key':'Name', 'Value':my_efs_name},
+                                                    {'Key':'Owner', 'Value':iam_user_name},
+                                                    {'Key':'Department', 'Value':'Computational Biology Research'}])
+
+
+# Now that the file system exists, create a mount target in each availability
+# zone. Only one mount target can be created per availability zone.
+#efscl.create_mount_target(FileSystemId=my_efs_id, SubnetId=my_subnetid_df.loc['us-east-1a','Public' ], SecurityGroups=[ my_security_group.id ])
+efscl.create_mount_target(FileSystemId=my_efs_id, SubnetId=my_subnetid_df.loc['us-east-1a','Private'], SecurityGroups=[ my_security_group.id ])
+#efscl.create_mount_target(FileSystemId=my_efs_id, SubnetId=my_subnetid_df.loc['us-east-1e','Public' ], SecurityGroups=[ my_security_group.id ])
+efscl.create_mount_target(FileSystemId=my_efs_id, SubnetId=my_subnetid_df.loc['us-east-1e','Private'], SecurityGroups=[ my_security_group.id ])
+
+
+
+################################################################################
+# Done. Now just display the mount targets.
+################################################################################
+
+# Display the IPv4 of the mount targets
+[m['IpAddress'] for m in efscl.describe_mount_targets(FileSystemId=my_efs_id)['MountTargets']]
+
+# Merge AvailabilityZone and IPv4 address into a single DataFrame for each mount
+# target involved in this EFS.
+my_azip_df = pd.merge(pd.DataFrame(efscl.describe_mount_targets(FileSystemId=my_efs_id)['MountTargets']), pd.DataFrame(ec2cl.describe_subnets()['Subnets']), left_on=None)[['AvailabilityZone','IpAddress']]
+
+# Construct the DNS server names and place them in a convenient DataFrame
+my_efsservers_df = pd.DataFrame({'ip':my_azip_df['IpAddress'].tolist(), 'dns_name':['', '']}, index=my_azip_df['AvailabilityZone'].tolist())
+my_efsservers_df['dns_name'] = [row[1] + '.' + my_efs_id + '.efs.' + row[1][:-1] + '.amazonaws.com' for row in my_azip_df.itertuples()]
+# Usage examples:
+# my_efsservers_df.loc['us-east-1a','dns_name']
+# 'us-east-1a.fs-eb1aaba2.efs.us-east-1.amazonaws.com'
+# my_efsservers_df.loc['us-east-1a','ip']
+# '10.50.250.202'
+
+
+
+################################################################################
+# Mounting
+################################################################################
+
+# sudo mkdir /mnt/e0
+# sudo chmod 777 /mnt/e0
+# sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 us-east-1a.fs-eb1aaba2.efs.us-east-1.amazonaws.com:/ /mnt/e0
+# sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-eb1aaba2.efs.us-east-1.amazonaws.com:/ /mnt/e0
+# sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 10.50.250.202:/ /mnt/e0
+
+
