606cf5341a7384890ea4b0ee11d9621cc5d5686a
Ivan Gregoretti <ivangreg@gmail.com>
2017-09-15 15:11:15 -0400

    formosa-09 -> formosa-10

--- createami.py
+++ createami.py
@@ -681,6 +681,56 @@ ec2re.Volume(  my_ec2volume_id  ).create_tags(Tags=[{'Key':'Name', 'Value':my_ec
 my_ec2instance.reload()
 jmespath.search('InstanceStatuses[*].[SystemStatus.Status,InstanceStatus.Status] | []', ec2cl.describe_instance_status(InstanceIds=[my_ec2instance.id]))
 
+# Another cycle. Creation of snapshot and image of formosa-09 and new instance formosa-10
+my_ec2instance.stop()
+my_ec2volume_id = my_ec2instance.block_device_mappings[0]['Ebs']['VolumeId']
+my_ec2volume_name = jmespath.search('[?Key==`Name`].Value | [0]', my_ec2instance.tags) + '-vol'
+ec2re.Volume(  my_ec2volume_id  ).create_tags(Tags=[{'Key':'Name', 'Value':my_ec2volume_name},{'Key':'Owner', 'Value':iam_user_name},{'Key':'Department', 'Value':'Computational Biology Research'}])
+my_ec2instance.wait_until_stopped()
+my_ec2snapshot = ec2re.create_snapshot( VolumeId=my_ec2volume_id, Description='Root device snapshot of Fedora 26 for Computational Biology' )
+my_ec2snapshot.create_tags(Tags=[{'Key':'Name', 'Value':jmespath.search('[?Key==`Name`].Value | [0]', my_ec2instance.tags) + '-snap'},{'Key':'Owner', 'Value':iam_user_name},{'Key':'Department', 'Value':'Computational Biology Research'}])
+
+my_ec2snapshot.wait_until_completed()
+my_ec2image = ec2re.register_image(
+    Name='formosa-09',
+    Description='Fedora 26 for Computational Biology',
+    Architecture='x86_64',
+    RootDeviceName='/dev/sda1',
+    BlockDeviceMappings=[
+        {
+            'DeviceName': '/dev/sda1',
+            'Ebs': {
+                'SnapshotId': my_ec2snapshot.id,
+                'DeleteOnTermination': True,
+                'VolumeType': 'gp2'
+            }
+        }
+    ],
+    VirtualizationType='hvm',
+)
+my_ec2image.create_tags(Tags=[{'Key':'Name', 'Value':my_ec2image.name},{'Key':'Platform', 'Value':'Fedora 26'},{'Key':'Owner', 'Value':iam_user_name},{'Key':'Department', 'Value':'Computational Biology Research'}])
+
+jmespath.search('Images[*] | [?!( Public )] | [?Tags[?Key==`Department` && Value==`Computational Biology Research`]]', ec2cl.describe_images())
+jmespath.search('Images[*] | [?!( Public )]', ec2cl.describe_images(Filters=[{'Name':'tag:Department', 'Values':['Computational Biology Research']}]))
+my_ec2instance.terminate()
+my_ec2image.wait_until_exists()
+my_keypair = ec2re.KeyPair('kp-compbio0')
+my_ec2instance = ec2re.create_instances(ImageId=my_ec2image.id,
+        MinCount=1, MaxCount=1,
+        KeyName=my_keypair.name,
+        InstanceType='r4.xlarge',
+        BlockDeviceMappings=[{'DeviceName':'/dev/sda1', 'Ebs':{'VolumeSize':60, 'DeleteOnTermination':True, 'VolumeType':'gp2'}}],
+        NetworkInterfaces=[{'SubnetId':my_subnetid_df.loc['us-east-1a','Public'], 'Groups':[my_security_group.id], 'DeviceIndex':0, 'AssociatePublicIpAddress':True}],
+        InstanceInitiatedShutdownBehavior='terminate'
+        )[0]
+my_ec2instance.create_tags(Tags=[{'Key':'Name', 'Value':'formosa-10'},{'Key':'Owner', 'Value':iam_user_name},{'Key':'Department', 'Value':'Computational Biology Research'}])
+my_ec2instance.wait_until_exists(Filters=[{'Name':'block-device-mapping.status','Values':['attached']}])
+my_ec2volume_id = my_ec2instance.block_device_mappings[0]['Ebs']['VolumeId']
+my_ec2volume_name = jmespath.search('[?Key==`Name`].Value | [0]', my_ec2instance.tags) + '-vol'
+ec2re.Volume(  my_ec2volume_id  ).create_tags(Tags=[{'Key':'Name', 'Value':my_ec2volume_name},{'Key':'Owner', 'Value':iam_user_name},{'Key':'Department', 'Value':'Computational Biology Research'}])
+my_ec2instance.reload()
+jmespath.search('InstanceStatuses[*].[SystemStatus.Status,InstanceStatus.Status] | []', ec2cl.describe_instance_status(InstanceIds=[my_ec2instance.id]))
+
 ### LEFT HERE ###
 
 
